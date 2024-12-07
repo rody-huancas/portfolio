2679ded309f0797a648cb3051c0ed902e88a054c
Rody Huancas <rodyhuancas.04@gmail.com>
2024-12-06 22:30:23 -0500

    Feat: copy email

--- src/components/home/RecentProjects.tsx
+++ src/components/home/RecentProjects.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export const RecentProjects = () => {
   return (
     <Card title="Proyectos Recientes" nameLink="Ver más" href="/">
-      <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-1 gap-7'>
+      <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-1 gap-7 pt-4'>
         <ImagePreview />
         <ImagePreview />
       </div>
--- src/components/home/Skills.tsx
+++ src/components/home/Skills.tsx
@@ -3,28 +3,28 @@ import { Card, IconFigma, IconDBMySql, IconDocker, IconExpress, IconGit, IconGra
 export const Skills = () => {
   return (
     <Card title="Mis Habilidades">
-      <div className="grid grid-cols-5 gap-5">
-        <IconJavaScript className="w-9 h-9" />
-        <IconTypeScript className="w-9 h-9" />
-        <IconPHP className="w-9 h-9" />
+      <div className="grid grid-cols-5 gap-3">
+        <IconJavaScript className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
+        <IconTypeScript className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
+        <IconPHP className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
 
-        <IconReact className="w-9 h-9" />
-        <IconNextJS className="w-9 h-9" />
-        <IconTailwindCSS className="w-9 h-9" />
+        <IconReact className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
+        <IconNextJS className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
+        <IconTailwindCSS className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
 
-        <IconNodeJS className="w-9 h-9" />
-        <IconNestJS className="w-9 h-9" />
-        <IconExpress className="w-9 h-9" />
-        <IconGraphQL className="w-9 h-9" />
-        <IconTypeOrm className="w-9 h-9" />
+        <IconNodeJS className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
+        <IconNestJS className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
+        <IconExpress className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
+        <IconGraphQL className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
+        <IconTypeOrm className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
 
-        <IconDBMySql className="w-9 h-9" />
-        <IconPostgreSQL className="w-9 h-9" />
-        <IconMongoDB className="w-9 h-9" />
+        <IconDBMySql className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
+        <IconPostgreSQL className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
+        <IconMongoDB className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
 
-        <IconGit className="w-9 h-9" />
-        <IconDocker className="w-9 h-9" />
-        <IconFigma className="w-9 h-9" />
+        <IconGit className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
+        <IconDocker className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
+        <IconFigma className="bg-gray-100 dark:bg-gray-100/20 p-2 rounded-xl w-12 h-12" />
       </div>
     </Card>
   )
